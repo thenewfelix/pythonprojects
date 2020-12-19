8d843d535972b30c796538d2095f24ab30020c60
Ibukun <awopetuid@gmail.com>
2020-12-19 19:25:42 +0100

    Second commit, with html template

--- EmailSender.py
+++ EmailSender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Your Name here'
 email['to'] = 'Receivers mail here'
 email['subject'] = 'Test for automated Email Sender'
 
-email.set_content('Your Content Here')
+email.set_content(html.substitute({'name': 'Receivers name'}), 'html')
   
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
